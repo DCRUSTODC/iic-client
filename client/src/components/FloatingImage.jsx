5cc99a79c1d342cd1981954f2830503e4318e8e5
mb-008 <97526225+mb-008@users.noreply.github.com>
2023-09-10 22:25:31 +0530

    Final Commit

--- client/src/components/FloatingImage.jsx
+++ client/src/components/FloatingImage.jsx
@@ -4,7 +4,7 @@ function FloatingImage() {
             <img
                 src=".\src\assets\event_images\ue.png"
                 alt="Floating Image"
-                className="w-13 h-12  shadow-md rounded-full shadow-lg shadow-white bg-cyan_secondary animate-fade-in animate-delay-500"
+                className="w-13 h-12  shadow-md rounded-full shadow-lg shadow-grey bg-cyan_secondary animate-fade-in animate-delay-500"
             />
         </div>
     );
