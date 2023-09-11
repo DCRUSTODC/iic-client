6e4898be38158d6bf76c659622e936a7cf2f6c98
mb-008 <97526225+mb-008@users.noreply.github.com>
2023-09-11 16:37:53 +0530

    Made the image transition smoother in event page

--- client/src/components/RotatingImages.jsx
+++ client/src/components/RotatingImages.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+
+// Import your images
 import image1 from '../assets/event_images/image1.jpg';
 import image2 from "../assets/event_images/image2.jpg";
 import image3 from '../assets/event_images/image3.jpg';
@@ -12,16 +14,28 @@ function RotatingImages() {
     const [imageOpacity, setImageOpacity] = useState(1);
 
     useEffect(() => {
-        const intervalId = setInterval(() => {
+        // Preload all images before starting the rotation
+        const preloadImages = imageSources.map((src) => {
+            const img = new Image();
+            img.src = src;
+            return img;
+        });
+
+        // Function to handle image rotation
+        const rotateImages = () => {
             setImageOpacity(0); // Fade out the image
             setTimeout(() => {
                 setCurrentImageIndex((prevIndex) =>
                     prevIndex === imageSources.length - 1 ? 0 : prevIndex + 1
                 );
                 setImageOpacity(1); // Fade in the new image
-            }, 400); // Delay the image change for 500 milliseconds
-        }, 3000); // Change image every 4 seconds
+            }, 400); // Delay the image change for 400 milliseconds
+        };
 
+        // Rotate images at regular intervals
+        const intervalId = setInterval(rotateImages, 3000); // Change image every 3 seconds
+
+        // Clear the interval when the component unmounts
         return () => clearInterval(intervalId);
     }, []);
 
