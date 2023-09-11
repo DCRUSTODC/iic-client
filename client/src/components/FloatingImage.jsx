5543200fe9dbe239436764e2708316adc6b86852
mb-008 <97526225+mb-008@users.noreply.github.com>
2023-09-11 13:35:35 +0530

    Fixed the path of floating image

--- client/src/components/FloatingImage.jsx
+++ client/src/components/FloatingImage.jsx
@@ -2,7 +2,7 @@ function FloatingImage() {
     return (
         <div className="floating-image">
             <img
-                src=".\src\assets\event_images\ue.png"
+                src="./src/assets/event_images/ue.png"
                 alt="Floating Image"
                 className="w-13 h-12  shadow-md rounded-full shadow-lg shadow-grey bg-cyan_secondary animate-fade-in animate-delay-500"
             />
