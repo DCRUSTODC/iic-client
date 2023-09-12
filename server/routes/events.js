f08de9ff0aee9095506bca78d9b2551614005344
Sambhav Saxena <sambhavsaxena02@gmail.com>
2023-09-12 08:53:11 +0530

    update: restructured file structure

--- server/routes/events.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getEvents, postEvent } from "../controllers/events.js";
-const router = express.Router();
-
-router.route("/").get(getEvents);
-router.route("/").post(postEvent);
-
-export default router;
