2e660c87233c418a504d62e5945318406e0a4212
thakurajay369 <ajthakur1205@gmail.com>
2023-09-23 20:35:49 +0530

    Linked Gallery

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Layout from './components/Layout';
 import Home from './pages/Home';
 import Team from './pages/Team';
 import Event from './pages/Event';
+import Gallery from './pages/Gallery';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route element={<Home />} index />
         <Route element={<Event />} path="/events" />
         <Route element={<Team />} path="/team" />
+        <Route element={<Gallery />} path="/gallery" />
       </Route>
     </Routes>
   );
