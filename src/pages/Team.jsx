a82633bf9c7701f8a841178d54b77acf53d2df9d
thakurajay369 <ajthakur1205@gmail.com>
2023-09-25 14:16:40 +0530

    Responsiveness added in team

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -13,7 +13,7 @@ export default function Team(){
             <div className="font-semibold text-cyan_secondary relative -top-1">IIC rely on these people</div>
         </center>
         <hr className="text-primary"/>
-        <h2 className="text-3xl font-bold m-3 text-accent">Faculty</h2>
+        <h2 className="text-3xl font-bold m-3 text-accent sm:text-left text-center">Faculty</h2>
         <center>
             <div className="flex flex-wrap flex-row justify-evenly items-center">
             {facs.map((fac)=>{
@@ -21,17 +21,17 @@ export default function Team(){
             })}
             </div>
         </center>
-        <h2 className="text-3xl font-bold m-3 text-accent">External Panel Expert</h2>
+        <h2 className="text-3xl font-bold m-3 text-accent sm:text-left text-center">External Panel Expert</h2>
         <ul className="my-5">
         {extern.map((p)=> {
             return (
-                <li key={p.id} className="text-cyan_secondary text-xl pl-24">
+                <li key={p.id} className="text-cyan_secondary text-xl sm:pl-24">
                     <span className="font-semibold text-white">{p.name}</span>, {p.desig}
                 </li>
             )
         })}
         </ul>
-        <h2 className="text-3xl font-bold m-3 text-accent">Student</h2>
+        <h2 className="text-3xl font-bold m-3 text-accent sm:text-left text-center">Student</h2>
         <TeamSection />
         </div>
     )
