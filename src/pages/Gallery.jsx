f719a84ee3fc739ffb3ed165bd36b5d6c23ea6df
thakurajay369 <ajthakur1205@gmail.com>
2023-09-26 18:51:14 +0530

    pulling changes

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,7 +1,18 @@
+import events from "../assets/json/events/data/events.json";
+import GalleryCard from "../components/GalleryCard";
+
 export default function Gallery(){
     return (
-        <div>
-
+        <div className="max-w-6xl mx-auto px-4 sm:px-6">
+            {events.map((event)=>{
+                return <GalleryCard
+                    title={event.title}
+                    date={event.date}
+                    desc={event.description}
+                    loc={event.location}
+                    images={event.images}
+                />
+            })}
         </div>
     );
 }
